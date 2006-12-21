89aa11398947958fd5b0504b57cd741d26cace1e
twm <twm@ef4fdce5-35f3-0310-a5ee-fc8da0176504>
2006-12-21 09:19:16 +0000

    adapted makedoc scripts to new directory structure

--- component/trunk/makedoc.cmd
+++ component/trunk/makedoc.cmd
@@ -2,6 +2,8 @@
 rem pasdoc.exe is from pasdoc.sourceforge.net
 
 mkdir docs
-utils\pasdoc --marker=: --output=docs --graphviz-uses --link-gv-uses=jpg --graphviz-classes --link-gv-classes=jpg *.pas
-dot -Grankdir=LR -T jpg docs\GVUses.dot > docs\GVUses.jpg
-dot -Grankdir=LR -T jpg docs\GVClasses.dot > docs\GVClasses.jpg
+cd source
+..\utils\pasdoc --marker=: --output=..\docs --graphviz-uses --link-gv-uses=jpg --graphviz-classes --link-gv-classes=jpg *.pas
+dot -Grankdir=LR -T jpg ..\docs\GVUses.dot > ..\docs\GVUses.jpg
+dot -Grankdir=LR -T jpg ..\docs\GVClasses.dot > ..\docs\GVClasses.jpg
+cd ..
