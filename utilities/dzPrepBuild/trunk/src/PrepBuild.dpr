9fdbc40661d2b1ed6125b8e73579d0d494f402c7
twm <twm@ef4fdce5-35f3-0310-a5ee-fc8da0176504>
2007-11-01 15:23:45 +0000

    adapted to new search paths and moving u_dzDefaultMain from dzlib to dzCmdLineParser

--- utilities/dzPrepBuild/trunk/src/PrepBuild.dpr
+++ utilities/dzPrepBuild/trunk/src/PrepBuild.dpr
@@ -14,10 +14,10 @@ uses
   u_DofVersionInfo in 'u_DofVersionInfo.pas',
   u_IniVersionInfo in 'u_IniVersionInfo.pas',
   u_CentralIniVersionInfo in 'u_CentralIniVersionInfo.pas',
-  u_dzDefaultMain in '..\libs\dzlib\src\u_dzDefaultMain.pas',
   u_PrepBuildMain in 'u_PrepBuildMain.pas',
   u_DummyVersionInfo in 'u_DummyVersionInfo.pas',
-  u_AbstractVersionInfo in 'u_AbstractVersionInfo.pas';
+  u_AbstractVersionInfo in 'u_AbstractVersionInfo.pas',
+  u_dzDefaultMain in '..\libs\dzCmdLineParser\src\u_dzDefaultMain.pas';
 
 {$R *_icon.res}
 {$R *_version.res}
