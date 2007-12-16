7d3dfab43d8b2e6f3444f89b45899df2f96fc016
twm <twm@ef4fdce5-35f3-0310-a5ee-fc8da0176504>
2007-12-16 15:35:48 +0000

    converted PasDoc comments to HelpInsight comments

--- utilities/dzLib/trunk/src/u_dzClassUtils.pas
+++ utilities/dzLib/trunk/src/u_dzClassUtils.pas
@@ -1,6 +1,8 @@
 {GXFormatter.config=twm}
-{: Implements functions which work on classes but are not methods.
-   @autor(twm) }
+/// <summary>
+/// Implements functions which work on classes but are not methods.
+/// @autor(twm)
+/// </summary>
 unit u_dzClassUtils;
 
 interface
@@ -12,74 +14,98 @@ uses
 // NOTE: The naming convention is <extended-class>_<Methodname>
 
 type
-  {: raised by StringByObj if no matching entry was found }
+  /// <summary>
+  /// raised by StringByObj if no matching entry was found
+  /// </summary>
   EObjectNotFound = class(Exception);
 
-  {: Removes trailing spaces from all lines in Strings as well as empty lines
-     from the end of Strings, returns true if at least one string was shortened
-     or an empty string was removed.
-     @param Strings is the TStrings class to work on.
-     @returns true, if something was changed, false otherwise }
+/// <summary>
+/// Removes trailing spaces from all lines in Strings as well as empty lines
+/// from the end of Strings, returns true if at least one string was shortened
+/// or an empty string was removed.
+/// @param Strings is the TStrings class to work on.
+/// @returns true, if something was changed, false otherwise
+/// </summary>
 function TStrings_RemoveTrailingSpaces(_Strings: TStrings): boolean;
 
-{: Free a TStrings object including all it's Object[n]s }
+/// <summary>
+/// Free a TStrings object including all it's Object[n]s
+/// </summary>
 procedure TStrings_FreeWithObjects(_Strings: TStrings);
 
-{: Frees all objects stored in the TStrings intance and returns the instance,
-   meant to be called like
-   @code( TStrings_FreeAllObjects(sl).Free; ) or
-   @code( TStrings_FreeAllObjects(sl).Clear; ) }
+/// <summary>
+/// Frees all objects stored in the TStrings intance and returns the instance,
+/// meant to be called like
+/// @code( TStrings_FreeAllObjects(sl).Free; ) or
+/// @code( TStrings_FreeAllObjects(sl).Clear; ) 
+/// </summary>
 function TStrings_FreeAllObjects(_Strings: TStrings): TStrings;
 
-{: frees the object and delets the entry from the list }
+/// <summary>
+/// frees the object and delets the entry from the list
+/// </summary>
 procedure TStrings_DeleteAndFreeObject(_Strings: TStrings; _Idx: integer);
 
-{: Free a TList object an all TObjects it contains
-   NOTE: this function is obsolete, use contnrs.TObjectList instead! }
+/// <summary>
+/// Free a TList object an all TObjects it contains
+/// NOTE: this function is obsolete, use contnrs.TObjectList instead!
+/// </summary>
 procedure TList_FreeWithItems(var _List: TList); deprecated; // use contnrs.TObjectList
 
-{: Write a string to the stream
-   @param Stream is the TStream to write to.
-   @param s is the string to write
-   @returns the number of bytes written. }
+/// <summary>
+// Write a string to the stream
+/// @param Stream is the TStream to write to.
+/// @param s is the string to write
+/// @returns the number of bytes written.
+/// </summary>
 function TStream_WriteString(_Stream: TStream; const _s: string): integer;
 
-{: Write a string to the stream appending CRLF
-   @param Stream is the TStream to write to.
-   @param s is the string to write
-   @returns the number of bytes written. }
+/// <summary>
+/// Write a string to the stream appending CRLF
+/// @param Stream is the TStream to write to.
+/// @param s is the string to write
+/// @returns the number of bytes written. 
+/// </summary>
 function TStream_WriteStringLn(_Stream: TStream; const _s: string): integer;
 
-{: Read a line from a stream, that is, a string ending in CRLF
-   @param Stream is the TStream to read from.
-   @param s returns the read string.
-   @returns the number of bytes read. }
+/// <summary>
+/// Read a line from a stream, that is, a string ending in CRLF
+/// @param Stream is the TStream to read from.
+/// @param s returns the read string, without the CRLF
+/// @returns the number of bytes read, excluding the CRLF
+/// </summary>
 function TStream_ReadStringLn(_Stream: TStream; out _s: string): integer;
 
-{: Write formatted data to the stream appending CRLF
-   @param Stream is the TStream to write to.
-   @param Format is a format string as used in sysutils.format
-   @param Args is an array of const as used in sysutils.format
-   @returns the number of bytes written. }
+/// <summary>
+/// Write formatted data to the stream appending CRLF
+/// @param Stream is the TStream to write to.
+/// @param Format is a format string as used in sysutils.format
+/// @param Args is an array of const as used in sysutils.format
+/// @returns the number of bytes written.
+/// </summary>
 function TStream_WriteFmtLn(_Stream: TStream; const _Format: string; _Args: array of const): integer;
 
-{: returns the string which has the given value as Object
-   @param Strings is the TStrings to search
-   @param Obj is a pointer to match
-   @param RaiseException is a boolean that controls whether an exception should
-          be raised (if true) if the Obj cannot be found or an empty strin should
-          be returned (if false), Default = true
-   @returns the string whose object matches Obj or an empty string, if none
-            was found and RaiseExeption was false
-   @raises EObjectNotFound if a matching object was not found and RaiseException
-                           is true. }
+/// <summary>
+/// returns the string which has the given value as Object
+/// @param Strings is the TStrings to search
+/// @param Obj is a pointer to match
+/// @param RaiseException is a boolean that controls whether an exception should
+///           be raised (if true) if the Obj cannot be found or an empty strin should
+///           be returned (if false), Default = true
+/// @returns the string whose object matches Obj or an empty string, if none
+///          was found and RaiseExeption was false
+/// @raises EObjectNotFound if a matching object was not found and RaiseException
+///         is true.
+/// </summary>
 function TStrings_StringByObj(_Strings: TStrings; _Obj: pointer; _RaiseException: boolean = true): string;
 
-{: determines the string which has the given value as Object
-   @param Strings is the TStrings to search
-   @param Obj is a pointer to match
-   @param Value is the string whose object matches Obj, only valid if result is true
-   @returns true, if a matching object was found, false otherwise }
+/// <summary>
+/// determines the string which has the given value as Object
+/// @param Strings is the TStrings to search
+/// @param Obj is a pointer to match
+/// @param Value is the string whose object matches Obj, only valid if result is true
+/// @returns true, if a matching object was found, false otherwise
+/// </summary>
 function TStrings_TryStringByObj(_Strings: TStrings; _Obj: pointer; out _Value: string): boolean;
 
 implementation
