42b94f4d1b4dcd701c66496d023319909fb893b7
twm <twm@ef4fdce5-35f3-0310-a5ee-fc8da0176504>
2009-09-25 14:38:04 +0000

    new method SplitConnectionString

--- utilities/dzLib/trunk/dzAdoDb/src/c_dzAdoConnection.pas
+++ utilities/dzLib/trunk/dzAdoDb/src/c_dzAdoConnection.pas
@@ -18,6 +18,15 @@ type
   TAdoServerType = (asUnknown, asJet, asMsSql, asOracle);
 
 type
+  TConnectionInfoRec = record
+    ServerType: TAdoServerType;
+    ServerName: string;
+    Database: string;
+    Username: string;
+    Parts: array of string;
+  end;
+
+type
   ///<summary> This TAdoConnection descendant solves the problem of Connections
   ///          staying open when opened in the designer. The Connected property
   ///          will never be saved to the .DFM file and therefore always be false.
@@ -29,6 +38,7 @@ type
     class function EditConnectionString(_ParentHandle: THandle; var _ConnectionString: string): boolean; overload;
     function DetermineServerType: TAdoServerType; overload;
     class function DetermineServerType(const _ConnectionString: string): TAdoServerType; overload;
+    class function SplitConnectionString(const _ConnectionString: string): TConnectionInfoRec;
   published
     procedure ClearConnectionObject;
     property Connected stored false;
@@ -71,21 +81,47 @@ var
   s: WideString;
 begin
   DataInit := CreateComObject(CLSID_DataLinks) as IDataInitialize;
-  if _ConnectionString <> '' then
-    begin
-      s := _ConnectionString;
-      DataInit.GetDataSource(nil, CLSCTX_INPROC_SERVER,
-        PWideChar(s), IUnknown, DataSource);
-    end;
+  if _ConnectionString <> '' then begin
+    s := _ConnectionString;
+    DataInit.GetDataSource(nil, CLSCTX_INPROC_SERVER,
+      PWideChar(s), IUnknown, DataSource);
+  end;
   DBPrompt := CreateComObject(CLSID_DataLinks) as IDBPromptInitialize;
   Result := Succeeded(DBPrompt.PromptDataSource(nil, _ParentHandle,
     DBPROMPTOPTIONS_PROPERTYSHEET, 0, nil, nil, IUnknown, DataSource));
-  if Result then
-    begin
-      InitStr := nil;
-      DataInit.GetInitializationString(DataSource, True, InitStr);
-      _ConnectionString := InitStr;
+  if Result then begin
+    InitStr := nil;
+    DataInit.GetInitializationString(DataSource, True, InitStr);
+    _ConnectionString := InitStr;
+  end;
+end;
+
+class function TdzAdoConnection.SplitConnectionString(const _ConnectionString: string): TConnectionInfoRec;
+var
+  sl: TStringList;
+  s: string;
+  i: Integer;
+begin
+  Result.ServerType := DetermineServerType(_ConnectionString);
+  sl := TStringList.Create;
+  try
+    sl.Delimiter := ';';
+    sl.StrictDelimiter := true;
+    sl.DelimitedText := _ConnectionString;
+    SetLength(Result.Parts, sl.Count);
+    for i := 0 to sl.Count - 1 do begin
+      Result.Parts[i] := sl[i];
+      s := sl.Names[i];
+      if SameText(s, 'User ID') then
+        Result.Username := sl.Values[s]
+      else if SameText(s, 'Data Source') then
+        Result.ServerName := sl.Values[s]
+      else if SameText(s, 'Initial Catalog') then
+        Result.Database := sl.Values[s];
     end;
+  finally
+    sl.Free;
+  end;
 end;
 
 function TdzAdoConnection.EditConnectionString(_ParentHandle: THandle): boolean;
