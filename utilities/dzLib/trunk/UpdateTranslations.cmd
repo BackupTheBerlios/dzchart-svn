ace04dbf8387382d7dc2a4ff0811ec3475f98e55
twm <twm@ef4fdce5-35f3-0310-a5ee-fc8da0176504>
2010-03-13 11:46:35 +0000

    adapted to path for buildtools

--- utilities/dzLib/trunk/UpdateTranslations.cmd
+++ utilities/dzLib/trunk/UpdateTranslations.cmd
@@ -7,18 +7,18 @@
 @rem   - French (partial)
 
 @rem extract from subdirectories src, forms, adodb, components, dbcreator, dzAdoDb, IniFileFormatter, lockfree
-buildtools\dxgettext *.dfm *.pas *.inc *.dpr -r -b src -b forms -b adodb -b components -b dbcreator -b dzAdoDb -b IniFileFormatter -b lockfree -o .
+..\..\buildtools\dxgettext *.dfm *.pas *.inc *.dpr -r -b src -b forms -b adodb -b components -b dbcreator -b dzAdoDb -b IniFileFormatter -b lockfree -o .
 
 @rem remove strings given in ignore.po
-buildtools\msgremove default.po -i ignore.po -o filtered.po
+..\..\buildtools\msgremove default.po -i ignore.po -o filtered.po
 
 @rem merge German translations
-buildtools\msgmerge --no-wrap --update translations\de\dzlib.po filtered.po
+..\..\buildtools\msgmerge --no-wrap --update translations\de\dzlib.po filtered.po
 
 @rem merge French translations
-buildtools\msgmerge --no-wrap --update translations\fr\dzlib.po filtered.po
+..\..\buildtools\msgmerge --no-wrap --update translations\fr\dzlib.po filtered.po
 
 @rem merge English "translations"
-buildtools\msgmerge --no-wrap --update translations\en\dzlib.po filtered.po
+..\..\buildtools\msgmerge --no-wrap --update translations\en\dzlib.po filtered.po
 
 pause
